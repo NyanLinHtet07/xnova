6275d9a0c70ed7c139fb3e0cb7916f7a9c7ccf68
Nyan Lin Htet <nlh@nordictec.com>
2025-02-18 22:14:32 +0630

    update update data

--- resources/js/Components/Bars/BarMenusNewComponent.tsx
+++ resources/js/Components/Bars/BarMenusNewComponent.tsx
@@ -2,7 +2,7 @@ import React, {Fragment, useEffect, useState} from "react";
 import InputLabel from "../InputLabel";
 import TextInput from "../TextInput";
 import axios from "axios";
-import { Menus } from "@/types/bar";
+import { MenuItems, Menus } from "@/types/bar";
 import { IconEdit, IconTrash } from '@tabler/icons-react';
 import { Dialog, Transition } from "@headlessui/react";
 
@@ -18,6 +18,7 @@ const BarMenusNewComponent:React.FC<BarMenusComponentProps> = ({
 }) => {
     const [title, setTitle] = useState('');
     const [menuId, setID] = useState<number | null>(null);
+    const [menuItemId, setItemID] = useState<number | null>(null);
     const [menuList, setMenuList] = useState<Menus[]>([]);
     const [visibleMenuId, setVisibleMenuId] = useState<number | null>(null);
     const [menuItemName, setMenuItemName] = useState('');
@@ -25,6 +26,7 @@ const BarMenusNewComponent:React.FC<BarMenusComponentProps> = ({
     const [menuItemImage, setMenuItemImage] = useState<File | null>(null);
 
     const [isOpen, setIsOpen] = useState(false);
+    const [isItemOpen, setIsItemOpen] = useState(false);
 
     const handleOpen = (data: Menus) => {
         
@@ -35,6 +37,14 @@ const BarMenusNewComponent:React.FC<BarMenusComponentProps> = ({
             //setIcon(data.icon);
             setIsOpen(true);
         }
+
+    const handleListOpen = (data: MenuItems) => {
+        setMenuItemName(data.name);
+        setMenuItemPrice(data.price.toString());
+        setItemID(data.id);
+
+        setIsItemOpen(true)
+    }
 
    const fetchMenus = async() => {
     try {
@@ -88,6 +98,39 @@ const BarMenusNewComponent:React.FC<BarMenusComponentProps> = ({
             setMenuItemImage(null);
             setVisibleMenuId(null);
             fetchMenus();
+        }
+        catch (error) {
+            console.error("Error adding menu item", error);
+          }
+    }
+
+    const updateItem = async () => {
+        // if(!menuItemName || !menuItemPrice ){
+        //     alert("Please enter item name, price")
+        //     return;
+        // }
+
+        const formData = new FormData();
+        //formData.append('menu_id', menuId.toString());
+        formData.append('name', menuItemName);
+        formData.append('price', menuItemPrice);
+        if (menuItemImage) {
+            formData.append('image', menuItemImage);
+        }
+
+        formData.append('_method', 'PUT');
+
+        try {
+            await axios.post(`/api/bar/menu/items/${menuItemId}`, formData, {
+                headers: {"Content-Type": "multipart/fprm-data"}
+            });
+
+            setMenuItemName('');
+            setMenuItemPrice('');
+            setMenuItemImage(null);
+            setVisibleMenuId(null);
+            fetchMenus();
+            setIsItemOpen(false)
         }
         catch (error) {
             console.error("Error adding menu item", error);
@@ -212,14 +255,16 @@ const BarMenusNewComponent:React.FC<BarMenusComponentProps> = ({
                             }
 
                             {menu.menuitems.map(item => (
-                            <div key={item.id} className="flex items-center justify-start p-3 mt-2 bg-white border rounded-lg">
+                            <div key={item.id} className="flex items-center justify-between p-3 mt-2 bg-white border rounded-lg">
                               
                                 <img src={item.image ? `/${item.image}` : "/nodata.jpg"}  alt={item.name} className="object-cover w-16 h-16 mr-3 rounded-md" />
                              
                                 <div className="flex ml-10 space-x-4">
                                 <h4 className="font-bold">{item.name}</h4>
                                 <p>Price: {item.price}</p>
+                                
                                 </div>
+                                <IconEdit size={24} color="green" onClick={() => handleListOpen(item)}/>
                             </div>
                             ))}
                            
@@ -276,6 +321,87 @@ const BarMenusNewComponent:React.FC<BarMenusComponentProps> = ({
                 <div className="flex justify-end col-span-2 pb-5 mr-10">
                             <button className="px-3 py-2 text-sm text-white rounded-lg bg-cyan-800" onClick={() => updateData() }>Update Menu Title</button>
                         </div>
+                        </div>
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
+                </Dialog>
+            </Transition>
+
+            <Transition appear show={isItemOpen} as={Fragment}>
+                <Dialog as="div" className="relative z-50" onClose={() => setIsItemOpen(false)}>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0 scale-95"
+                        enterTo="opacity-100 scale-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100 scale-100"
+                        leaveTo="opacity-0 scale-95"
+                    >
+                        <div className="fixed inset-0 bg-black bg-opacity-30" />
+                    </Transition.Child>
+
+                    <div className="fixed inset-0 flex items-center justify-center p-4">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+                            <Dialog.Panel className="w-7/12 p-6 bg-white rounded-lg shadow-xl ">
+                                <Dialog.Title className="text-lg font-semibold">
+                                    Edit Item
+                                </Dialog.Title>
+                                <div className="grid grid-cols-2 gap-4 mx-8">
+                                <div>
+                                    <InputLabel value="Item Name" />
+                                    <TextInput 
+                                    type="text" 
+                                    value={menuItemName} 
+                                    onChange={(e) => setMenuItemName(e.target.value)} 
+                                    className="block w-full mt-1"
+                                    />
+                                </div>
+
+                                <div>
+                                <InputLabel value="Price" />
+                                <TextInput 
+                                type="number" 
+                                value={menuItemPrice} 
+                                onChange={(e) => setMenuItemPrice(e.target.value)} 
+                                className="block w-full mt-1"
+                                />
+                                </div>
+
+                                <div>
+                                <InputLabel value="Item Image" />
+                                    <input 
+                                    type="file" 
+                                    accept="image/*" 
+                                    onChange={(e) => setMenuItemImage(e.target.files?.[0] || null)} 
+                                    className="block w-full mt-1"
+                                    />
+                                </div>
+                               
+                                
+                                
+
+                                
+                                <div>
+                                <button 
+                                className="px-3 py-3 mt-4 text-xs text-white rounded-lg bg-cyan-800"
+                                onClick={() => updateItem()}
+                                >
+                                Save Item
+                                </button>
+                                </div>
+                
+                
+               
                         </div>
                             </Dialog.Panel>
                         </Transition.Child>
